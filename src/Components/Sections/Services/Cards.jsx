0f86032f0ce6532130f9feb2728b326121c5aea8
mehran78 <mehran.abaci@gmail.com>
2026-06-06 16:47:01 +0500

    Update Cards.jsx

--- src/Components/Sections/Services/Cards.jsx
+++ src/Components/Sections/Services/Cards.jsx
@@ -5,8 +5,9 @@ function Cards() {
     // <div className="flex justify-center">
     <div className="flex justify-center items-center gap-3 flex-col px-12 sm:px-24 md:flex-row md:px-12">
       <Card text={"Buy & Sell in CDA Sectors"} />
-      <Card text={"Park View City Installments"} />
-      <Card text={"Market Analysis and Evaluation"} />
+      <Card text={" Residentail & Commercial "} />
+      <Card text={"Market Analysis and Evaluation"}
+     <Card text={"  Rentals Management"}/>
     </div>
   );
 }
