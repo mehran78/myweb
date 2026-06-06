89a644961dc0384bcf979137e494ec6f15786702
mehran78 <mehran.abaci@gmail.com>
2026-06-06 14:56:49 +0500

    Update Cards.jsx

--- src/Components/Sections/Insights and Testimonials/Insights/Cards.jsx
+++ src/Components/Sections/Insights and Testimonials/Insights/Cards.jsx
@@ -5,8 +5,11 @@ import image2 from "../../../../Images/stat-4.jpg";
 function Cards() {
   return (
     <div className="flex flex-col justify-center px-4 gap-3 sm:px-8 sm:gap-6 md:px-12 md:gap-6 md:max-w-[70dvw] lg:px-4 lg:gap-3">
-      <InsightCard cardImage={image1} />
-      <InsightCard cardImage={image2} />
+      <InsightCard cardImage={image1}
+        cardText="skip the middle man ."
+        />
+      <InsightCard cardImage={image2}
+        cardtext ="demmo one two three"/>
     </div>
   );
 }
