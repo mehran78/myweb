3ed5396a3e08ce3bb7711925cef855f666e873a4
mehran78 <mehran.abaci@gmail.com>
2026-06-06 14:41:32 +0500

    Update InsightCard.jsx

--- src/Components/Sections/Insights and Testimonials/Insights/InsightCard.jsx
+++ src/Components/Sections/Insights and Testimonials/Insights/InsightCard.jsx
@@ -1,6 +1,6 @@
 import Button from "../../../Navbar/Button";
 
-function InsightCard({ cardImage }) {
+function InsightCard({ cardImage, text }) {
   return (
     <div className="flex flex-col sm:flex-row bg-red-300 rounded-md shadow-xl overflow-hidden bg-white lg:max-w-[45dvw] xl:max-w-[35dvw]">
       <img className="sm:max-w-48 w-full" src={cardImage}></img>
@@ -9,6 +9,7 @@ function InsightCard({ cardImage }) {
           Skip the middleman and the guesswork. Buy, sell, or rent residential and commercial real estate in Islamabad with absolute legal protection. Explore pre-vetted plots, houses, and commercial floors optimized for maximum returns and immediate ownership transfer.
 
 Select your transactional goal below to start dealing with the capital's most trusted real estate network.
+          {text}
         </p>
         <Button
           text="View More >"
